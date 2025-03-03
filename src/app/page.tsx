4a0f58c464720af12cc40430772df06c882c126e
jane7166 <122887267+jane7166@users.noreply.github.com>
2025-03-03 21:24:32 +0900

    제발좀되어라

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
   const router = useRouter();
   const [isClient, setIsClient] = useState(false);
 
